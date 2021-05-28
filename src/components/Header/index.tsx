2ca08c175e00b23a57feb927b8bb318257d79027
Emerson Pereira da Silva <emerson.pereirada@compasso.com.br>
2021-05-28 20:26:21 -0300

    :heavy_plus_sign: Add react-modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
-
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransitionModal: () => void;
+}
+export function Header({ onOpenNewTransitionModal }: HeaderProps) {
   return (
     <Container>Header
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova Transação</button>
+        <button
+          type="button"
+          onClick={onOpenNewTransitionModal}
+        >
+          Nova Transação
+        </button>
       </Content>
     </Container>
   )
